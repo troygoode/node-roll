8b47c4bacb67224229e48dda22b328a054e99764
Troy Goode <troygoode@gmail.com>
2012-12-27 18:20:38 -0500

    travis

--- test/works.js
+++ test/works.js
@@ -1,7 +1,7 @@
 var should = require('should')
   , srand = require('srand')
   , roll = require('../lib')
-  , randomSet = [
+  , randomSet = [ // set of random numbers I'll pull from
     .24,
     .62
   ]
@@ -19,13 +19,11 @@ describe('roll', function(){
 
   it('addition works', function(){
     var result = roll.roll('2d20+3');
-    console.log(result);
     result.result.should.equal(21);
   });
 
   it('subtraction works', function(){
     var result = roll.roll('2d20-3');
-    console.log(result);
     result.result.should.equal(15);
   });
 });
