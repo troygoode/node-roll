d9e9b0eca1a8eee7e6361966adb6128338b24f16
Troy Goode <troygoode@gmail.com>
2014-10-30 11:58:32 -0700

    passing lint

--- test/works.js
+++ test/works.js
@@ -1,41 +1,54 @@
-var should = require('should')
-  , srand = require('srand')
-  , roll = require('../lib')
-  , randomSet = [ // set of random numbers I'll pull from
-    .24,
-    .62
-  ]
-  , randomPointer = 0;
+/*jslint indent: 2*/
+/*global require: true, module: true, describe: true, it: true, beforeEach: true */
+(function () {
+  'use strict';
 
-// can only test this library if we make things not random
-roll.random = function(){
-  return randomSet[randomPointer++];
-};
+  var should = require('should'),
+    Roll = require('../lib'),
+    randomSet = [ // set of random numbers I'll pull from
+      0.24,
+      0.62
+    ],
+    randomPointer = 0,
+    roll = new Roll(function () {
+      // can only test this library if we make things not random
+      randomPointer = randomPointer + 1;
+      return randomSet[randomPointer - 1];
+    });
 
-describe('roll', function(){
-  beforeEach(function(){
-    randomPointer = 0;
+  describe('roll', function () {
+    beforeEach(function () {
+      randomPointer = 0;
+    });
+
+    it('addition works', function () {
+      var result = roll.roll('2d20+3');
+      result.result.should.equal(21);
+    });
+
+    it('subtraction works', function () {
+      var result = roll.roll('2d20-3');
+      result.result.should.equal(15);
+    });
+
+    it('validates input', function (done) {
+      try {
+        roll.roll('garbage');
+        done('Should not be reachable.');
+      } catch (e) {
+        should.exist(e);
+        e.name.should.eql('InvalidInputError');
+        e.message.should.eql('"garbage" is not a valid input string for node-roll.');
+        e.input.should.eql('garbage');
+        done();
+      }
+    });
+
+    it('exposes validation', function () {
+      roll.validate('2d20-3').should.equal(true);
+      roll.validate('garbage').should.equal(false);
+    });
   });
 
-  it('addition works', function(){
-    var result = roll.roll('2d20+3');
-    result.result.should.equal(21);
-  });
-
-  it('subtraction works', function(){
-    var result = roll.roll('2d20-3');
-    result.result.should.equal(15);
-  });
-
-  it('validates input', function(){
-    (function(){
-      roll.roll('garbage');
-    }).should.throw(roll.InvalidInputError);
-  });
-
-  it('exposes validation', function(){
-    roll.validate('2d20-3').should.equal(true);
-    roll.validate('garbage').should.equal(false);
-  });
-});
+}());
 
