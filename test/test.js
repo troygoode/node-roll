7ce794a5685690baeccd6959b7eb03a987de28a5
cajahnke <cajahnke@users.noreply.github.com>
2015-09-08 09:52:58 -0500

    change rolled plus fix paths for win32

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@
 
   var exec = require('child_process').exec,
     should = require('should'),
+    fs = require('fs'),
     Roll = require('../lib'),
     FakeRandom = require('./fake-random'),
     random = new FakeRandom([ // can only test this library if we make things not actually random
@@ -47,22 +48,22 @@
     it('d20', function () {
       var result = roll.roll('d20');
       result.rolled.length.should.equal(1);
-      result.rolled[0].should.equal(5);
+      result.rolled[0][0].should.equal(5);
       result.result.should.equal(5);
     });
 
     it('d%', function () {
       var result = roll.roll('d%');
       result.rolled.length.should.equal(1);
-      result.rolled[0].should.equal(25);
+      result.rolled[0][0].should.equal(25);
       result.result.should.equal(25);
     });
 
     it('2d20', function () {
       var result = roll.roll('2d20');
-      result.rolled.length.should.equal(2);
-      result.rolled[0].should.equal(5);
-      result.rolled[1].should.equal(13);
+      result.rolled[0].length.should.equal(2);
+      result.rolled[0][0].should.equal(5);
+      result.rolled[0][1].should.equal(13);
       result.result.should.equal(18);
     });
 
@@ -131,7 +132,7 @@
     });
 
     it('bin/roll garbage', function (done) {
-      exec(__dirname + '/../bin/roll garbage', function (err, stdout, stderr) {
+      exec((process.platform === 'win32' ? 'node ././bin/roll' : __dirname + '/../bin/roll') + ' garbage', function (err, stdout, stderr) {
         if (err) {
           should.exist(err);
           stderr.should.eql('"garbage" is not a valid input string for node-roll.\n');
@@ -142,7 +143,7 @@
     });
 
     it('bin/roll 2d20', function (done) {
-      exec(__dirname + '/../bin/roll 2d20', function (err, stdout, stderr) {
+      exec((process.platform === 'win32' ? 'node ././bin/roll' : __dirname + '/../bin/roll') + ' 2d20', function (err, stdout, stderr) {
         if (err) {
           return done(err);
         }
@@ -153,4 +154,3 @@
 
   });
 }());
-
